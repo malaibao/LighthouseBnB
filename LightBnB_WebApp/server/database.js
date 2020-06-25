4d0a658c8f7ce2f59b9b81152583c276539f1526
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-25 12:41:30 -0300

    added db queries for getting reservations with guest_id joining properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -65,7 +65,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  const queryStr = `SELECT * FROM reservations WHERE guest_id=$1 LIMIT $2`;
+  const queryStr = `
+  SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating
+  FROM properties
+      JOIN reservations ON reservations.property_id = properties.id
+      JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1 AND
+      now()::date > end_date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
   return pool.query(queryStr, [guest_id, limit]).then((dbRes) => {
     console.log(dbRes.rows);
     return dbRes.rows;
