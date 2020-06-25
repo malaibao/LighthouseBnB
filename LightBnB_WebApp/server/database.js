b9a51da97244b85fd6bf1642c69ec6a5811ba960
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-25 13:30:28 -0300

    construct strings for getting prperties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -91,8 +91,69 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const queryStr = `SELECT * FROM properties LIMIT $1`;
-  return pool.query(queryStr, [limit]).then((dbRes) => dbRes.rows);
+  const queryVal = [];
+  let queryStr = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+
+  // Check if options obj contains city
+  if (options.city) {
+    queryVal.push(`%${options.city}%`);
+    queryStr += `WHERE city LIKE $${queryVal.length} `;
+  }
+
+  // Check if option obj contains owner_id
+  if (options.owner_id) {
+    queryVal.push(options.owner_id);
+    if (queryStr.includes('WHERE')) {
+      queryStr += `AND properties.owner_id=$${queryVal.length}`;
+    } else {
+      queryStr += `
+  WHERE properties.owner_id=$${queryVal.length}`;
+    }
+  }
+
+  // Check if option obj contains minimum_price_night and maximum_price_night
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryVal.push(
+      options.minimum_price_per_night,
+      options.maximum_price_per_night
+    );
+    if (queryStr.includes('WHERE')) {
+      queryStr += `AND properties.cost_per_night BETWEEN $${
+        queryVal.length - 1
+      } AND $${queryVal.length}`;
+    } else {
+      queryStr += `
+      WHERE properties.cost_per_night BETWEEN $${queryVal.length - 1} AND $${
+        queryVal.length
+      }`;
+    }
+  }
+
+  // add the GROUP BY clause
+  queryStr += `
+  GROUP BY properties.id`;
+
+  // Check if options obj contains minimum rating
+  if (options.minimum_rating) {
+    queryVal.push(options.minimum_rating);
+    queryStr += `
+  HAVING avg(property_reviews.rating) >= $${queryVal.length}`;
+  }
+
+  // Push limit
+  queryVal.push(limit);
+  queryStr += `
+  ORDER BY cost_per_night
+  LIMIT $${queryVal.length};
+  `;
+
+  console.log(queryStr);
+  console.log(queryVal);
+
+  return pool.query(queryStr, queryVal).then((dbRes) => dbRes.rows);
 };
 exports.getAllProperties = getAllProperties;
 
