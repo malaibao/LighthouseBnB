e70c42a2cd050084b606bb5ade0a00c863bd3e80
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-25 16:38:59 -0300

    fixed the star rating display

--- LightBnB_WebApp/public/javascript/components/property_listing.js
+++ LightBnB_WebApp/public/javascript/components/property_listing.js
@@ -1,7 +1,11 @@
 $(() => {
   window.propertyListing = {};
-  
+
   function createListing(property, isReservation) {
+    const rating =
+      property.average_rating !== null
+        ? `${Math.round(property.average_rating * 100) / 100}/5 stars`
+        : 'No rating';
     return `
     <article class="property-listing">
         <section class="property-listing__preview-image">
@@ -14,18 +18,23 @@ $(() => {
             <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
             <li>parking_spaces: ${property.parking_spaces}</li>
           </ul>
-          ${isReservation ? 
-            `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
-            : ``}
+          ${
+            isReservation
+              ? `<p>${moment(property.start_date).format('ll')} - ${moment(
+                  property.end_date
+                ).format('ll')}</p>`
+              : ``
+          }
           <footer class="property-listing__footer">
-            <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
-            <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            <div class="property-listing__rating">${rating}</div>
+            <div class="property-listing__price">$${
+              property.cost_per_night / 100.0
+            }/night</div>
           </footer>
         </section>
       </article>
-    `
+    `;
   }
 
   window.propertyListing.createListing = createListing;
-
 });
